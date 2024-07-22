20a8b42f4e4744be89519eb96572a29dc6b422a9
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-07-22 14:02:42 +0900

    1-2

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Comment from "../components/Comment";
 
 export default function DetailPage(): JSX.Element {
-  return <div>detail</div>;
+  return (
+    <div>
+      <div>작성자</div>
+      <div>내용</div>
+      <div>2021-12-12</div>
+      <div>
+        댓글 목록
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <input />
+        <button>댓글 쓰기</button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function LoginPage(): JSX.Element {
-  return <div>login</div>;
+  return (
+    <div>
+      <button>카카오 로그인</button>
+    </div>
+  );
 }
